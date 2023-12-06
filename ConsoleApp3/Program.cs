ec4ac8486d4811b91ac2dd57222389b811f0e903
Esmaeil1378 <esmaeilrezaei3781222@gmail.com>
2023-12-06 09:28:26 +0330

    edited by esmaeil

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,3 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
+
+for (int i = 0; i < 4; i++)
+{
+    Console.WriteLine("rbawerhq3ejh");
+}
